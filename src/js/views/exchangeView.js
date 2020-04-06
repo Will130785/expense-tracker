31399c3a71a61da1a7764420237d31aafff90465
willconstable <will_constable@msn.com>
2020-04-06 15:21:33 +0100

    API altered and countries amended

--- src/js/views/exchangeView.js
+++ src/js/views/exchangeView.js
@@ -4,7 +4,7 @@ import {elements} from "./base";
 export const displayRates = (rate) => {
 
     elements.ukpDisplay.textContent = rate.data.rates.GBP;
-    elements.eurDisplay.textContent = rate.data.rates.EUR;
+    elements.eurDisplay.textContent = rate.data.rates.HUF;
     elements.usdDisplay.textContent = rate.data.rates.USD;
     elements.candDisplay.textContent = rate.data.rates.CAD;
     elements.tbhtDisplay.textContent = rate.data.rates.THB;
